97554cc205c588ea741832046cce7848efa51d12
Junheng Zheng <jz7259@g.rit.edu>
2025-04-06 17:39:19 -0400

    readme and added modal information

---
+++ readMe.txt
@@ -0,0 +1,16 @@
+ISTE 340 Project Read Me
+------------------------
+
+Inspiration
+-The official ischool website 
+
+Techstack 
+-React
+-vite
+-tailwind CSS
+
+Components Used
+-custom accordian comp. and courses modal
+-bootstrap for tabs and people modal
+-mui for paginated table
+
--- src/components/peopleComponents/peopleModal.jsx
+++ src/components/peopleComponents/peopleModal.jsx
@@ -46,10 +46,27 @@ export default function PeopleModal({ prop, handleOpen }) {
               {prop.name}
             </p>
             <p className="m-0 text-[16px]">{prop.title}</p>
+            <p className="m-0 text-[16px]">{prop.tagline}</p>
+            <p className="m-0 text-[14px]">
+              Interest Area | {prop.interestArea}
+            </p>
             <hr className="my-2"></hr>
-            <p className="m-0 text-gray-700 text-[14px]">{prop.office}</p>
-            <p className="m-0 text-gray-700 text-[14px]">{prop.phone}</p>
-            <p className="m-0 text-gray-700 text-[14px]">{prop.website}</p>
+            <p className="m-0 text-gray-700 text-[14px]">
+              <strong>Office | </strong>
+              {prop.office}
+            </p>
+            <p className="m-0 text-gray-700 text-[14px]">
+              <strong>Phone | </strong>
+              {prop.phone}
+            </p>
+            <p className="m-0 text-gray-700 text-[14px]">
+              <strong>Email | </strong>
+              {prop.email}
+            </p>
+            <p className="m-0 text-gray-700 text-[14px]">
+              <strong>Website| </strong>
+              {prop.website}
+            </p>
           </div>
         </Box>
       </Modal>
